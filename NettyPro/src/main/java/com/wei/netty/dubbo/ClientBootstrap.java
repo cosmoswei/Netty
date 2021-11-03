753c04b0e9b4db21b05396d5a4efd21bd414eaa2
宇宙伟 <49271049+cosmoswei@users.noreply.github.com>
2021-11-03 17:24:40 +0800

    day6 commit

--- NettyPro/src/main/java/com/wei/netty/dubbo/ClientBootstrap.java
+++ NettyPro/src/main/java/com/wei/netty/dubbo/ClientBootstrap.java
@@ -12,7 +12,6 @@ public class ClientBootstrap {
             Thread.sleep(2 * 1000);
             String res = service.hello("hello dubbo~");
             System.out.println("result= " + res);
-            System.out.println(i);
         }
     }
 }
